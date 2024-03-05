3ed3068dfb82ec3906562fb0f62dabe1ee516007
Théotime <tronaidev@gmail.com>
2024-03-05 09:22:00 -0500

    adding @import local and external function

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const io = require('./src/io.js');
+const io = require('./src/modules/io.js');
 const AST = require('./src/abstractSyntaxTree.js');
+const axios = require('axios');
 
-// On vérifie les arguments.
-const args = process.argv.slice(2);
-if (args.length != 2 || !(new RegExp('.*\.cssfly$').test(args[0])) || !(new RegExp('.*\.css$').test(args[1])))
-        throw new Error("Invalid arguments. Use : cssfly <input.cssfly> <output.css>")
+async function main() {
+        // On vérifie les arguments.
+        const args = process.argv.slice(2);
+        if (args.length != 2 || !(new RegExp('.*\.cssfly$').test(args[0])) || !(new RegExp('.*\.css$').test(args[1])))
+                throw new Error("Invalid arguments. Use : cssfly <input.cssfly> <output.css>")
 
-data = io.readFileContent(args[0]);
-const a = new AST(data, args[0]);
-a.parse();
-io.writeFileContent(args[1], a.astToCSS());
+        // on transforme le fichier, à update...
+        data = io.readFileContent(args[0]);
+        const a = new AST(data, args[0]);
+        await a.parse();
+        io.writeFileContent(args[1], a.astToCSS());
+}
+
+
+main();
 
 
---
+++ src/modules/import.js
@@ -0,0 +1,65 @@
+const axios = require('axios');
+const io = require('./io.js');
+
+
+async function importFile(data, path) {
+    
+    let regex = new RegExp('^@import\\([\"\'](.*)[\"\']\\);?');
+    let tableau = [];
+    let toReplace;
+
+    data = data.replace(regex, (match, a1) => { tableau.push(a1); return a1; });
+
+    for (let i = 0; i < tableau.length; i++) {
+        console.log(tableau[i]);
+        if (tableau[i].startsWith("http")) {
+            toReplace = await importExternalFile(tableau[i]);
+        } else {
+            toReplace = importLocalFile(data, path, tableau[i]);
+        }
+
+        data = data.replace(tableau[i], toReplace);
+
+    }
+
+    return data;
+}
+    
+async function importExternalFile(a1) {
+    try {
+        const response = await axios.get(a1); // Attendre la réponse de axios
+        return response.data; // Retourner les données reçues
+    } catch (error) {
+        console.error('Error:', error);
+        return null; // Retourner null ou gérer autrement en cas d'erreur
+    }
+}
+
+/**
+ * Permet d'importer des fichiers local.
+ * @param {*} data le css appelant l'import
+ * @param {*} path le chemin du fichier css appelant
+ * @param {*} a1 le chemin du fichier à importer
+ * @returns le css après importation
+ */
+function importLocalFile(data, path, a1) {
+    
+    let content = "";
+    
+    try {
+        let lastSlashIndex = path.lastIndexOf('/');
+        let directoryPath = path.substring(0, lastSlashIndex + 1);
+        content = io.readFileContent(directoryPath + a1);
+    } catch (error) {
+        content = "/* Impossible d'import le fichier. */";
+    } finally {
+        return content;
+    }
+        
+}
+
+module.exports = {importFile};
+
+
+
+
