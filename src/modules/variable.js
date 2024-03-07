4fa2c0bd4753d2da1230fd7d074f5d1b5278b950
Théotime <tronaidev@gmail.com>
2024-03-06 21:41:20 -0500

    Documentation variable.js

--- src/modules/variable.js
+++ src/modules/variable.js
@@ -1,3 +1,11 @@
+/**
+ * Permet de calculer une fonction.
+ * ex: 1 + 1 => 2
+ * ex: true && false => false
+ * @param {*} exp l'expression à évaluer.
+ * @param {*} ast image du système au moment
+ * @returns 
+ */
 function evaluate(exp, ast) {
 
     exp = replaceVar(exp, ast);
@@ -6,6 +14,7 @@ function evaluate(exp, ast) {
         return eval(exp);
     } catch (error) {
         if (error instanceof ReferenceError) {
+            // Si l'erreur est de type reference, cela veut dire que c'est un string.
             if (exp.startsWith('(') && exp.endsWith(')')) exp = exp.substring(1, exp.length - 1);
             return exp;
         } else {
@@ -14,15 +23,24 @@ function evaluate(exp, ast) {
     }
 }
 
+/**
+ * Permet de remplacer les variables par leurs valeurs (récursivement).
+ * @param {*} exp expression à remplacer.
+ * @param {*} ast image du systeme à ce moment.
+ * @returns 
+ */
 function replaceVar(exp, ast) {
+
     let newExp = exp.replace(new RegExp('\\$([a-zA-Z0-9]*)'), (match, a1) => {
         let brk = true;
         let value = "<Error>";
+
         for (let i = ast.length; i >= 0 && brk; i--) {
             if (ast[i]) {
-                if (ast[i].var.some(e => e.name == a1.replace(/\$/, ''))) {
+                a1 = a1.replace(/\$/, '');
+                if (ast[i].var.some(e => e.name == a1)) {
                     ast[i].var.map(o => {
-                        if (o.name == a1.replace(/\$/, '')) {
+                        if (o.name == a1) {
                             brk = false;
                             value = "(" + o.value + ")";
                         }
@@ -32,7 +50,6 @@ function replaceVar(exp, ast) {
         }
 
         return value;
-
     });
 
     if (newExp === exp) return newExp;
